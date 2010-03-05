446363a44dc053f71565f99d8fc504148513e0bf
Ludovic CLAIN <ludovic.clain@rbs.fr>
2010-03-05 11:11:50 +0000

    [FIX #23523] édition de locales

--- actions/EditLocaleAction.class.php
+++ actions/EditLocaleAction.class.php
@@ -1,7 +1,6 @@
 <?php
 class uixul_EditLocaleAction extends f_action_BaseAction
 {
-
 	/**
 	 * @param Context $context
 	 * @param Request $request
@@ -11,5 +10,32 @@ class uixul_EditLocaleAction extends f_action_BaseAction
         $request->setAttribute('moduleName', $this->getModuleName($request));
 
 		return View::INPUT;
+	}
+	
+	/**
+	 * Initialize this action.
+	 *
+	 * @param Context The current application context.
+	 * @return bool true, if initialization completes successfully, otherwise false.
+	 */
+	public function initialize($context)
+	{
+		$result = parent::initialize($context);
+		
+		// Force to ckeck the permission on the good module instead of uixul.
+		// FIXME: find a better way to do that...
+		$request = $context->getRequest();
+		if ($request->hasParameter('wemod'))
+		{
+			$module = $request->getParameter('wemod');
+		}
+		else 
+		{
+			throw new Exception('no module selcted');
+		}
+		
+		$request->setAttribute(K::EFFECTIVE_MODULE_NAME, array($module));
+		
+		return $result;
 	}
 }
