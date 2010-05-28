ead50ed637837d612a4f6629eda844a2d425981b
Eric HAUSWALD <eric.hauswald@rbs.fr>
2010-05-28 13:47:53 +0000

    [FIX] correction du nombres d'argument de la fonction getParameters des commands

--- changedev-commands/default/AddDocumentEditor.php
+++ changedev-commands/default/AddDocumentEditor.php
@@ -33,7 +33,7 @@ class commands_AddDocumentEditor extends commands_AbstractChangedevCommand
 	 * @param array<String, String> $options where the option array key is the option name, the potential option value or true
 	 * @return String[] or null
 	 */
-	function getParameters($completeParamCount, $params, $options)
+	function getParameters($completeParamCount, $params, $options, $current)
 	{
 		if ($completeParamCount == 0)
 		{
