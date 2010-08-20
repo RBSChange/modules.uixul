3004a885f91b9b1cea3328ef9694245e1dfe24dc
Eric HAUSWALD <eric.hauswald@rbs.fr>
2010-08-20 15:23:52 +0200

    [UPDATE] Remplace le fichier cacheversion.txt par l'action uixul_GetCacheversion

---
+++ actions/GetCacheVersionAction.class.php
@@ -0,0 +1,18 @@
+<?php
+class uixul_GetCacheVersionAction extends f_action_BaseJSONAction
+{
+	/**
+	 * @param Context $context
+	 * @param Request $request
+	 */
+	public function _execute($context, $request)
+	{
+		controller_ChangeController::setNoCache();
+		$cacheVersion = $this->getPersistentProvider()->getSettingValue('modules_uixul', 'cacheVersion');
+		if ($cacheVersion === null)
+		{
+			$cacheVersion = 0;
+		}
+		return $this->sendJSON(array('cacheVersion' => $cacheVersion));
+	}
+}
