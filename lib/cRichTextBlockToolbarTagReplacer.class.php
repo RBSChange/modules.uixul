3b38e3893e4022a907c5f064239dadcb7c67f04d
Gaël PORT <gael.port@rbs.fr>
2011-09-26 10:41:55 +0200

    [NEW] Richtext.xml simplification.

--- lib/cRichTextBlockToolbarTagReplacer.class.php
+++ lib/cRichTextBlockToolbarTagReplacer.class.php
@@ -3,54 +3,14 @@ class uixul_lib_cRichTextBlockToolbarTagReplacer extends f_util_TagReplacer
 {
 	protected function preRun()
 	{
+		$ls = LocaleService::getInstance();
 		$addons = array();
-		foreach (uixul_RichtextConfigService::getInstance()->getConfigurationArray() as $configDeclaration)
+		foreach (uixul_RichtextConfigService::getInstance()->getConfigurationArray() as $styleInfos)
 		{
-			// Skip declarations without label.
-			if ($configDeclaration[uixul_RichtextConfigService::LABEL_ATTRIBUTE_NAME] == '')
-			{
-				continue;
-			}
-			
-			$tag = $configDeclaration[uixul_RichtextConfigService::TAG_ATTRIBUTE_NAME];
-			if (isset($configDeclaration[uixul_RichtextConfigService::ATTR_ATTRIBUTE_NAME]))
-			{
-				if (isset($configDeclaration[uixul_RichtextConfigService::ATTR_ATTRIBUTE_NAME]['class']))
-				{
-					$tag .= "." . $configDeclaration[uixul_RichtextConfigService::ATTR_ATTRIBUTE_NAME]['class'];
-				}
-			}
-			$label = $configDeclaration[uixul_RichtextConfigService::LABEL_ATTRIBUTE_NAME];
-			$module = isset($configDeclaration['module']) ? $configDeclaration['module'] : '';
-			$document = isset($configDeclaration['document']) ? $configDeclaration['module'] : '';
-			if (isset($configDeclaration['module']))
-			{
-				$module = ' module="' . $configDeclaration['module'] . '"';
-				if (isset($configDeclaration['document']))
-				{
-					$document = ' document="' . $configDeclaration['module'] . '"';
-				}
-				else
-				{
-					$document = ' document="all"';
-				}
-			}
-			else
-			{
-				$module = '';
-				$document = '';
-			}
-			if (isset($configDeclaration['block']) && ($configDeclaration['block'] == "false"))
-			{
-				$command = 'surround';
-			}
-			else
-			{
-				$command = 'formatblock';
-			}
-			
-			$addons[] = sprintf('<menuitem anonid="%s"%s%s type="checkbox" autocheck="false" label="%s" oncommand="applyStyle(\'%s\', \'%s\')"/>', $tag, $module, $document, f_Locale::translateUI($label), $command, $tag);
-			
+			$tag = $styleInfos['tag'] . '.' . $styleInfos['class'];
+			$label = isset($styleInfos['labeli18n']) ? $ls->transBO($styleInfos['labeli18n'], array('ucf')) : $styleInfos['label'];
+			$command = ($styleInfos['block']) ? 'formatblock' : 'surround';
+			$addons[] = sprintf('<menuitem anonid="%s" type="checkbox" autocheck="false" label="%s" oncommand="applyStyle(\'%s\', \'%s\')"/>', $tag, f_Locale::translateUI($label), $command, $tag);
 		}	
 	
 		if (count($addons))
--- lib/services/RichtextConfigService.class.php
+++ lib/services/RichtextConfigService.class.php
@@ -1,15 +1,26 @@
 <?php
 class uixul_RichtextConfigService extends BaseService
 {
-	const TAG_ATTRIBUTE_NAME = "tag";
-	const LABEL_ATTRIBUTE_NAME = "label";
-	const ATTR_ATTRIBUTE_NAME = "attributes";
+	/**
+	 * @deprecated use the value directly.
+	 */
+	const TAG_ATTRIBUTE_NAME = 'tag';
 	
+	/**
+	 * @deprecated use the value directly.
+	 */
+	const LABEL_ATTRIBUTE_NAME = 'label';
+	
+	/**
+	 * @deprecated use the value directly.
+	 */
+	const ATTR_ATTRIBUTE_NAME = 'attributes';
+
 	/**
 	 * @var uixul_RichtextConfigService
 	 */
 	private static $instance;
-	
+
 	/**
 	 * @return uixul_RichtextConfigService
 	 */
@@ -21,7 +32,7 @@ class uixul_RichtextConfigService extends BaseService
 		}
 		return self::$instance;
 	}
-	
+
 	/**
 	 * @return Array
 	 */
@@ -34,41 +45,62 @@ class uixul_RichtextConfigService extends BaseService
 			if (!$domDoc->load($file))
 			{
 				Framework::error(__METHOD__ . ": $file is not a valid XML");
+				continue;
 			}
 			$styles = $domDoc->getElementsByTagName('style');
 			foreach ($styles as $style)
 			{
-				if (!$style->hasAttribute(self::TAG_ATTRIBUTE_NAME))
+				$props = array();
+				
+				/* @var $style DOMElement */
+				$tagName = $style->getAttribute('tag');
+				if (!$tagName)
 				{
-					Framework::error(__METHOD__ . ": style has no " . self::TAG_ATTRIBUTE_NAME . " attribute");
+					Framework::error(__METHOD__ . ": style has no valid 'tag' attribute");
+					continue;
+				}
+				$props['tag'] = $tagName;
+				
+				$className = $style->getAttribute('class');
+				if (!$className)
+				{
+					Framework::error(__METHOD__ . ": style has no valid 'class' attribute");
+					continue;
+				}
+				$props['class'] = $className;
+
+				$labelI18n = $style->getAttribute('labeli18n');
+				$label = $style->getAttribute('label');
+				if ($labelI18n)
+				{
+					$props['labeli18n'] = $labelI18n;
+				}
+				elseif ($label)
+				{
+					$props['label'] = $label;					
+				}
+				else
+				{
+					Framework::error(__METHOD__ . ": style has no valid 'labeli18n' nor 'label' attribute");
 					continue;
 				}
 				
-				$tagName = $style->getAttribute(self::TAG_ATTRIBUTE_NAME);
-				$label = $style->getAttribute(self::LABEL_ATTRIBUTE_NAME);
-				$props = array(self::TAG_ATTRIBUTE_NAME => $tagName, self::LABEL_ATTRIBUTE_NAME => $label);
-				foreach ($style->attributes as $attrName => $attrNode)
-				{
-					if ($attrName == self::TAG_ATTRIBUTE_NAME || $attrName == self::LABEL_ATTRIBUTE_NAME || $attrName == self::ATTR_ATTRIBUTE_NAME)
-										{
-						continue;
-					}
-					$props[$attrName] = $attrNode->nodeValue;
-				}
-				$props[self::ATTR_ATTRIBUTE_NAME] = $this->buildAttributes($style, $config);
-				$config[] = $props;
+				$block = $style->getAttribute('class');
+				$props['block'] = ($block != 'false');
+				
+				$config[$tagName.'.'.$className] = $props;
 			}
 		}
 		return $config;
 	}
-	
+
 	/**
-	 * @return Array
+	 * @return array
 	 */
 	protected function getConfigurationFilePaths()
 	{
 		$result = array();
-		$moduleList = array('generic', 'website');
+		$moduleList = array('website', 'richtext'); // website module contains static definitions and richtext contains dynamic ones.
 		foreach ($moduleList as $module)
 		{
 			$path = $this->resolveConfigurationFilesForModule($module);
@@ -87,31 +119,5 @@ class uixul_RichtextConfigService extends BaseService
 	private function resolveConfigurationFilesForModule($moduleName)
 	{
 		return FileResolver::getInstance()->setPackageName('modules_' . $moduleName)->setDirectory('config')->getPath('richtext.xml');
-	}
-	
-	/**
-	 * @param DOMElement $styleNode
-	 * @param array $config
-	 */
-	private function buildAttributes($styleNode)
-	{
-		$attrs = array();
-		$attributes = $styleNode->getElementsByTagName('attribute');
-		foreach ($attributes as $attribute)
-		{
-			if (!$attribute->hasAttribute("name"))
-			{
-				Framework::error(__METHOD__ . ": attribute has no name");
-				continue;
-			}
-			
-			if (!$attribute->hasAttribute("value"))
-			{
-				Framework::error(__METHOD__ . ": attribute has no value");
-				continue;
-			}
-			$attrs[$attribute->getAttribute("name")] = $attribute->getAttribute("value");
-		}
-		return $attrs;
 	}
 }
