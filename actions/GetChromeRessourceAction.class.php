8c96792ad1d16d7058cfd03fca9b8449f283384a
Gaël PORT <gael.port@rbs.fr>
2013-06-24 14:15:21 +0200

    [DEPRECATE] GetChromeRessourceAction

--- actions/GetChromeRessourceAction.class.php
+++ actions/GetChromeRessourceAction.class.php
@@ -1,36 +1,15 @@
 <?php
 /**
+ * @deprecated
  */
 class uixul_GetChromeRessourceAction extends f_action_BaseAction
 {
 	/**
-	 * @param Context $context
-	 * @param Request $request
+	 * @deprecated
 	 */
 	public function _execute($context, $request)
 	{
-		
-		$path = $request->getParameter('path');
-		$filename = f_util_FileUtils::buildDocumentRootPath($path);
-		if (is_readable($filename))
-		{
-			header("Expires: " . gmdate("D, d M Y H:i:s", time()+28800) . " GMT");
-			if (f_util_FileUtils::getFileExtension($filename) == 'css')
-			{
-				header('Content-type: text/css');
-			}
-			else
-			{
-				$finfo = finfo_open(FILEINFO_MIME, Framework::getConfigurationValue("modules/media/fileinfo_magic_file_path", "/usr/share/file/magic")); // return mime type ala mimetype extension
-		    	header('Content-type: ' . finfo_file($finfo, $filename));
-				finfo_close($finfo);
-			}
-			readfile($filename);			
-		}
-		else
-		{
-			f_web_http_Header::setStatus(404);
-		}
-		return View::NONE;
+		Framework::fatal('DEPRECATED Call to uixul_GetChromeRessourceAction');
+		die;
 	}
 }
